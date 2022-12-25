1eadd1d3bca59dbb1214d6b2a0ea0496d530b4af
V-Blaze <valentine6586@gmail.com>
2022-12-26 00:11:26 +0100

    Add experties section to the Home Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar';
 export { default as Sidebar } from './Sidebar';
 export { default as Profile } from './Profile';
+export { default as Experties } from './Experties';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,70 @@
 import React from 'react';
+import { MdDesktopMac } from 'react-icons/md';
+import { FaReact, FaPeopleArrows } from 'react-icons/fa';
 
+// Components
+import { Experties } from '../components';
+
+// Assets
 import { pProfile } from '../assets';
+// import { reactIcon } from '../languages';
 
 const Home = () => (
-  <div className=" grid md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 mb-4">
-    <div className="mt-12">
-      <div className=" bg-green-200 w-fit rounded-lg">
-        <h1 className="my-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent border-[#fff] border-2 border-solid px-2 w-fit rounded-lg">Hi There! I&apos;m Valentine</h1>
+  <>
+    <div className=" grid md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 my-20 ">
+      <div className="mt-12">
+        <div className=" bg-green-200 w-fit rounded-lg">
+          <h1 className="my-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent border-[#fff] border-2 border-solid px-2 w-fit rounded-lg">Hi There! I&apos;m Valentine</h1>
+        </div>
+        <h3 className="text-4xl font-epilogue text-white font-extrabold leading-normal ">
+          A
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> FullStack Engineer</span>
+          . I Help Startups
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Launch </span>
+          And
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Grow </span>
+          Their Products
+        </h3>
+        <span className=" text-[#A3B3BC] text-xl font-epilogue my-3 block ">
+          I am a software engineer with more than four years of experience. recognized
+          as a practical and effective developer, experienced in leading cross-functional
+          teams in a time-pressured setting to complete projects on schedule and within budget.
+        </span>
       </div>
-      <h3 className="text-4xl font-epilogue text-white font-extrabold leading-normal ">
-        A
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> FullStack Engineer</span>
-        . I Help Startups
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Launch </span>
-        And
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Grow </span>
-        Their Products
-      </h3>
-      <span className=" text-[#A3B3BC] text-xl font-epilogue my-3 block ">
-        I am a software engineer with more than four years of experience. recognized
-        as a practical and effective developer, experienced in leading cross-functional
-        teams in a time-pressured setting to complete projects on schedule and within budget.
-      </span>
+      <div className="flex justify-center items-center">
+        <img src={pProfile} alt="profile" />
+      </div>
     </div>
-    <div className="flex justify-center items-center">
-      <img src={pProfile} alt="profile" />
+    <div className="flex flex-col justify-center items-center my-20">
+      <h3 className="text-5xl underline font-epilogue text-white font-extrabold leading-4 my-8">My Experties</h3>
+      <div className="grid grid-cols-1 sm:gap-3 lg:gap-0 md:gap-0 md:grid-cols-2 lg:grid-cols-3">
+        <Experties
+          title="BackEnd Development"
+          Icon={MdDesktopMac}
+          desc="
+          Experienced in both functional and OOP: Ruby, Rails, Ruby on Rails, Node,
+          JavaScript, TypeScript.
+          "
+        />
+        <Experties
+          title="FrontEnd Develpoment"
+          Icon={FaReact}
+          desc="
+          Passionate about UI/UX. Over 4 years of development
+          experience in HTML, CSS, Figma, JS, React, Redux, and NextJS frameworks.
+          "
+        />
+        <Experties
+          title="Soft Skills"
+          Icon={FaPeopleArrows}
+          desc="
+          Excellent in Remote collaboration, pair programming, agile methodologies,
+          communication, mentorship, leadership.
+          "
+        />
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
