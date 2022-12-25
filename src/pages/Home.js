8d30db4f230c43a4314082743b5bf8b9d993979c
V-Blaze <valentine6586@gmail.com>
2022-12-25 16:51:02 +0100

    Add personal introduction section to the Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const Home = () => (
-  <div>Home</div>
+  <div className="flex mx-2">
+    <div className="mt-12">
+      <div className=" bg-green-200 w-fit rounded-lg">
+        <h1 className="my-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent border-[#fff] border-2 border-solid px-2 w-fit rounded-lg">Hi There! I&apos;m Valentine</h1>
+      </div>
+      <h3 className="text-4xl font-epilogue text-white font-extrabold leading-normal ">
+        A
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> FullStack Engineer</span>
+        . I Help Startups
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Launch </span>
+        And
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4ca5ff] to-[#b673f8] bg-transparent"> Grow </span>
+        Their Products
+      </h3>
+      <span className=" text-yellow-50 text-xl font-epilogue my-3 block ">
+        I am a software engineer with more than four years of experience. recognized
+        as a practical and effective developer, experienced in leading cross-functional
+        teams in a time-pressured setting to complete projects on schedule and within budget.
+      </span>
+    </div>
+  </div>
 );
 
 export default Home;
